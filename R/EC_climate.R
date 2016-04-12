9f02d883bda0c0e190881bd80edde9f500acd9ad
Chris McConnell <cris.mcc76@gmail.com>
2016-04-12 14:49:12 -0400

    Added an example. Clear up issues with the script when switching between timeframes.

--- R/EC_climate.R
+++ R/EC_climate.R
@@ -1,13 +1,11 @@
 library(lubridate)
 
-# WebID, timeframe (timeframe = 1: for hourly data, timeframe = 2: for daily
+# StationID, timeframe (timeframe = 1: for hourly data, timeframe = 2: for daily
 # data, timeframe = 3 for monthly data), start date and end date
 
-envcan <- function(WebID, startDate , endDate, timeframe = 2) {
+envcan <- function(StationID, firstYear, lastYear, timeframe = 2) {
 
-  start = ymd(startDate)
-  end = ymd(endDate)
-  years = seq(start, end, 'years')
+  years = seq(firstYear, lastYear)
 
   h <- if (timeframe == 1) {
     16
@@ -17,37 +15,47 @@ envcan <- function(WebID, startDate , endDate, timeframe = 2) {
     18
   }
 
+  m <- if (timeframe == 1) {
+    1:12
+  } else if (timeframe == 2) {
+    1
+  } else if (timeframe == 3) {
+    1
+  }
+
   weather_data = c()
 
   for (i in 1:length(years)) {
-    year <- year(years[i])
-    month <- month(years[i])
+    year <- years[i]
+    for (i in 1:length(m)) {
+      month <- m[i]
 
-    URL <- paste(
-      'http://climat.weather.gc.ca/climateData/bulkdata_e.html?format=csv&stationID=',
-      WebID,
-      '&Year=',
-      year,
-      '&Month=',
-      month,
-      '&Day=14&timeframe=',
-      timeframe,
-      '&submit= Download+Data',
-      sep = ''
-    )
+      URL <- paste(
+        'http://climat.weather.gc.ca/climateData/bulkdata_e.html?format=csv&stationID=',
+        StationID,
+        '&Year=',
+        year,
+        '&Month=',
+        month,
+        '&Day=14&timeframe=',
+        timeframe,
+        '&submit= Download+Data',
+        sep = ''
+      )
 
-    data <- read.table(
-      URL,
-      header = TRUE,
-      sep = ",",
-      skip = h,
-      fill = TRUE,
-      stringsAsFactors = FALSE
-    )
+      data <- read.table(
+        URL,
+        header = TRUE,
+        sep = ",",
+        skip = h,
+        fill = TRUE,
+        stringsAsFactors = FALSE
+      )
 
-    if (is.data.frame(data)) {
-      # Collate Data
-      weather_data <- rbind(weather_data, data)
+      if (is.data.frame(data)) {
+        # Collate Data
+        weather_data <- rbind(weather_data, data)
+      }
     }
   }
   return(weather_data)
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # envcan
- Download data in bulk from Environmnet Canada Climate Website
+Download data in bulk from Environmnet Canada Climate Website. Based on URL based procedure to automatically download data in bulk from Climate Website. List of available data is located here.  (ftp://ftp.tor.ec.gc.ca/Pub/Get_More_Data_Plus_de_donnees/)
+
+```{R}
+ECStations = read.table(
+    "ftp://client_climate@ftp.tor.ec.gc.ca/Pub/Get_More_Data_Plus_de_donnees/Station%20Inventory%20EN.csv", 
+    sep = ",", 
+    skip = 3,
+    header = TRUE 
+)
+```
+
+You can reference the EC station list after grabing from the Environmnet Canada ftp site using the row index of the station your interested in. In this case 5825 for the Muskoka airport.
+
+```{R}
+MUSKOKA <- envcan(StationID = ECStations$Station.ID[5825], 
+    firstYear = ECStations$DLY.First.Year[5825], 
+    lastYear = ECStations$DLY.Last.Year[5825], timeframe = 2)
+```
